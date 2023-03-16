30ec110ab935d94f6fdc9bbde555e3335c4e35ae
rabbit <2455056179@qq.com>
2023-03-16 20:31:12 +0800

    实现根据分类id删除分类

--- reggie_take_out/src/main/java/com/rabbit/reggie/controller/CategoryController.java
+++ reggie_take_out/src/main/java/com/rabbit/reggie/controller/CategoryController.java
@@ -1,17 +1,62 @@
 package com.rabbit.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.rabbit.reggie.common.R;
+import com.rabbit.reggie.entity.Category;
 import com.rabbit.reggie.service.CategoryService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 分类的管理
  */
 @RestController
 @RequestMapping("/category")
+@Slf4j
 public class CategoryController {
 
     @Autowired
     private CategoryService categoryService;
+
+    /**
+     * 新增分类
+     * @param category
+     * @return
+     */
+    @PostMapping
+    public R<String> save(@RequestBody Category category){
+        log.info("category:{}",category);
+        categoryService.save(category);
+        return R.success("新增分类成功");
+    }
+
+    @GetMapping("/page")
+    public R<Page> page(int page,int pageSize){
+        //分页构造器
+        Page<Category> pageInfo = new Page<>(page,pageSize);
+        //条件构造器
+        LambdaQueryWrapper<Category> queryWrapper = new LambdaQueryWrapper<>();
+        //添加排序条件，根据sort进行排序
+        queryWrapper.orderByAsc(Category::getSort); //getSort是升序排序
+
+        //进行分页查询
+        categoryService.page(pageInfo,queryWrapper);
+
+        return R.success(pageInfo);
+    }
+
+    /**
+     * 根据id删除分类
+     * @param ids
+     * @return
+     */
+    @DeleteMapping
+    public R<String> delete(String name, Long ids){
+        log.info("删除分类：{}，id为：{}",name,ids);
+
+        categoryService.removeById(ids);
+        return R.success("分类信息删除成功！");
+    }
 }
